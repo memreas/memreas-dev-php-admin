2b2a4190b2826a595de2083a3915fa6202937b6c
john meah <johnmeah@memreas.com>
2014-06-03 16:27:56 -0400

    3-JUN-2014 changed admin email in config file

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -12,7 +12,6 @@
  */
 
 return array(
-
     // Whether or not to enable a configuration cache.
     // If enabled, the merged configuration will be cached and used in
     // subsequent requests.
@@ -23,59 +22,14 @@ return array(
     //'cache_dir' =>  './data/cache',
     // ...
 
-        'db'=> array(
-        //mysql --host=aa19n8yspndox3g.co0fw2snbu92.us-east-1.rds.amazonaws.com --user=memreasdbuser --password=memreas2013 memreasbackenddb
-            'adapters'=>array(
-                'memreasdevdb' => array(
-                        'dsn'      => 'mysql:dbname=jhon;host=localhost',
-                        'username' => 'root',
-                        'password' => '',
-                        'driver'         => 'Pdo',
-                ),
-                'memreasbackenddb' => array(
-                        'dsn'            => 'mysql:dbname=memreasbackenddb;host=aa19n8yspndox3g.co0fw2snbu92.us-east-1.rds.amazonaws.com',
-                        'username' => 'memreasdbuser',
-                        'password' => 'memreas2013'
-                ),
-                'memreasintdb' => array(
-                'driver' => 'Pdo',
-                       'dsn'      => 'mysql:dbname=jhon;host=localhost',
-                        'username' => 'root',
-                        'password' => '',
-                         
-                'driver_options' => array(
-                    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-                ),
-            ),
-/*
-                'memreasdevdb' => array(
-                        'dsn'      => 'mysql:dbname=memreasdevdb;host=localhost',
-                        'username' => 'root',
-                        'password' => 'john1016'
-                ),
-                'memreasbackenddb' => array(
-                        'dsn'      => 'mysql:dbname=memreasbackenddb;host=localhost',
-                        'username' => 'root',
-                        'password' => 'john1016'
-                ),
-*/
-            )
-        ),
-    'doctrine' => array(
-        'connection' => array(
-            'orm_default' => array(
-                'params' => array(
-                                        'host' => 'localhost',
-
-                    'user'     => 'root',
-                    'password' => '',
-                     
-                    'port' => '3306',
-                    'dbname' => 'jhon',
-                    
+                'db'=> array(
+                        'adapters'=>array(
+                                'memreasintdb' => array(
+                                                'dsn'      => 'mysql:dbname=memreasintdb;host=memreasdev-db.co0fw2snbu92.us-east-1.rds.amazonaws.com',
+                                                'username' => 'memreasdbuser',
+                                                'password' => 'memreas2013'
+                                ),
+                        )
                 )
-            )
-        )
-    ),
-
 );
+
