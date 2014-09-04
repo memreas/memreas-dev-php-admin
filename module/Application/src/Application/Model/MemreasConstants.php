a2572fe2084faf65d4370538c62fa7c7982680aa
kamlesh-pawar <kamleshpawar2000@yahoo.com>
2014-09-04 17:08:01 +0530

    fix a admin pages

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -7,8 +7,8 @@ namespace Application\Model;
 
 class MemreasConstants {
 	
-//const MEMREAS_WS = "http://memreasdev-wsu.elasticbeanstalk.com/";
-  const MEMREAS_WS = "http://ws/";
+const MEMREAS_WS = "http://memreasdev-wsu.elasticbeanstalk.com/";
+ // const MEMREAS_WS = "http://ws/";
 
 	const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
         
