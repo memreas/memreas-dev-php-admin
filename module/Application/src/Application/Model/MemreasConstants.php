57ff1a96eec586266e5d9bbe9fd9c1ac93449a9d
kamlesh-pawar <kamleshpawar2000@yahoo.com>
2014-09-19 11:37:50 +0530

    fix signed url

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -11,10 +11,11 @@ const MEMREAS_WS = "https://memreasdev-wsu.memreas.com/";
   //const MEMREAS_WS = "http://ws/";
 
 	const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
-        
-        	const S3HOST = 'https://s3.amazonaws.com/';
+    const S3HOST = 'https://s3.amazonaws.com/';
 	const S3BUCKET = "memreasdev";
 	const TOPICARN = "arn:aws:sns:us-east-1:004184890641:us-east-upload-transcode-worker-int";
+	const SIGNURLS = true;
+	const EXPIRES = 36000;
 
 
 	
--- module/Application/src/Application/View/Helper/Mem.php
+++ module/Application/src/Application/View/Helper/Mem.php
@@ -2,18 +2,22 @@
 namespace Application\View\Helper;
  use Zend\View\Helper\AbstractHelper;
 use Application\Model\MemreasConstants;
-  
+use Application\View\Helper\MemreasSignedURL;
+
 class Mem extends AbstractHelper
 {
     public $sm;
     public function __construct($sm) {
         $this->sm;
+        $this->signer = new MemreasSignedURL();
     }
     public function ProfilePic($metadata="") {
         $json_array = json_decode ( $metadata, true );
         $url = '/img/profile-pic.jpg';
-        if (! empty ( $json_array ['S3_files']['thumbnails']['79x80'])){
-            $url = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $json_array ['S3_files']['thumbnails']['79x80'];
+        if (! empty ( $json_array ['S3_files']['path'])){
+            $url = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $json_array ['S3_files']['path'];
+            $url = json_decode($this->signer->signArrayOfUrls($url));
+            $url = $url[0];
         }
         return $url ;
     }
@@ -21,8 +25,10 @@ class Mem extends AbstractHelper
         $json_array = json_decode ( $metadata, true );
         $url = '/img/small-pic-3.jpg';
         if (! empty ( $json_array ['S3_files'] ['path'] )){
-            $url = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST . $url98x78 = $json_array ['S3_files']['thumbnails']['98x78'];
-        }
+            $url = MemreasConstants::CLOUDFRONT_DOWNLOAD_HOST .  $json_array ['S3_files']['path'];
+			$url = json_decode($this->signer->signArrayOfUrls($url));
+            $url = $url[0];
+ } 
         return $url;
     }
     public function showDate($date="") {
