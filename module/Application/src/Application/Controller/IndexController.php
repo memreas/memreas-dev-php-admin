91774321c4dd772df96e0aeff1ae0ed36af4b1f1
kamlesh-pawar <kamleshpawar2000@yahoo.com>
2014-09-29 12:11:50 +0530

    tring to fix error

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -401,7 +401,7 @@ public function getUserTable() {
         error_reporting(E_ALL);
 ini_set('display_errors', '1');
  $url = 'https://memreasdev-wsu.memreas.com';
- print_r(get_headers($url));
+ error_log('headers:'.print_r(get_headers($url),true));
  $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_FAILONERROR,true);
