1896465e348928525a365dfc2956c55eae341a90
kamlesh <kamleshpawar2000@yahoo.com>
2016-02-22 08:59:55 +0530

    .

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1185,7 +1185,7 @@ class IndexController extends AbstractActionController {
 						<limit>15</limit>
 						</getorderhistory></xml>" );
 			$orderData = simplexml_load_string ( $result );
-			echo '<pre>';print_r($result);
+			echo '<pre>';var_dump($result);
 			return array (
 					'orderData' => $orderData,
 					'page' => $page 
