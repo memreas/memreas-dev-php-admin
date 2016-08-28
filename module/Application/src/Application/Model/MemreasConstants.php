54269fcb0c7c9b241cb6bd11d7675d8ae0a1e7aa
john meah <johnmeah@memreas.com>
2016-08-28 17:10:31 -0400

    update for new redis server

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -7,11 +7,12 @@
 namespace Application\Model;
 
 class MemreasConstants {
-	//const MEMREAS_WS = "https://memreasdev-wsa.memreas.com";
+	#const MEMREAS_WS = "https://memreasprod-ws.memreas.com";
 	const MEMREAS_WS = "http://127.0.0.1:55152";
 	
 	// Redis constant section
-	const REDIS_SERVER_ENDPOINT = "10.164.216.184";
+	//const REDIS_SERVER_ENDPOINT = "10.154.58.83";
+	const REDIS_SERVER_ENDPOINT = "10.142.17.134";
 	const REDIS_SERVER_USE = true;
 	const REDIS_SERVER_SESSION_ONLY = true;
 	const REDIS_SERVER_PORT = "6379";
@@ -19,17 +20,17 @@ class MemreasConstants {
 	const REDIS_CACHE_USER_TTL = 300; // 5 minutes
 	
 	// s3 section
-	const S3BUCKET = "memreasdevsec";
-	const S3_APPKEY = 'AKIAIXDH3Q23AP4KPZRA';
-	const S3_APPSEC = 'TM+p8s9hlJwnKECCEKSVVhEOBjVhTWDG4ijIhJdk';
-	const S3HLSBUCKET = "memreasdevhlssec";
+	const S3_APPKEY = 'AKIAISDIQFVJMWFYXCIA';
+	const S3_APPSEC = 'eM5HG4MbYhkW1Jz1RWIdMapo2s+DbB+KnkhzTt91';
+	const S3BUCKET = "memreasprodsec";
+	const S3HLSBUCKET = "memreasprodhlssec";
 	const S3HOST = 'https://s3.amazonaws.com/';
 	const SIGNURLS = true;
 	const EXPIRES = 36000; // 10 hours
 	
 	// cloudfront section
-	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d3sisat5gdssl6.cloudfront.net/';
-	const CLOUDFRONT_HLSSTREAMING_HOST = 'https://d2cbahrg0944o.cloudfront.net/';
+	const CLOUDFRONT_DOWNLOAD_HOST = 'https://d321rfaqc9owi4.cloudfront.net/';
+	const CLOUDFRONT_HLSSTREAMING_HOST = 'https://d1fhgtf97i7jlq.cloudfront.net/';
 	const CLOUDFRONT_KEY_FILE = '/key/pk-APKAISSKGZE3DR5HQCHA.pem';
 	const CLOUDFRONT_KEY_PAIR_ID = 'APKAISSKGZE3DR5HQCHA';
 	const CLOUDFRONT_EXPIRY_TIME = 36000; // 10 hours
